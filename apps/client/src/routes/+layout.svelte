120e14629ad5c81bad3d4f96a996041be0243bae
thel3onardo <leonardo.contato03@gmail.com>
2023-09-12 10:36:51 -0300

    client(chore): install and setup tauri

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
   import "../app.css";
+
+  let counter = 0;
+
+  const handleClick = () => {
+    counter++;
+  };
 </script>
 
 <slot />
+<span>{counter}</span>
+<button on:click={handleClick} class="bg-red-500/30 px-4 py-2"
+  >click here</button
+>
