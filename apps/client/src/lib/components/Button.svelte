b3a06c71fa30a3f2bcd0d6df840710d9df684733
thel3onardo <leonardo.contato03@gmail.com>
2023-09-13 09:35:10 -0300

    feat(client): implement variant and rounded button props

--- apps/client/src/lib/components/Button.svelte
+++ apps/client/src/lib/components/Button.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-  export let background = false;
+  type VariantList = Record<string, string>;
+  type VariantKeyProp = keyof typeof variantList;
+
+  type RoundedList = Record<string, string>;
+  type RoundedKeyProp = keyof typeof roundedList;
+
+  const variantList = {
+    light: 'bg-white px-8 py-3 shadow-lg',
+    dark: 'bg-transparent shadow-lg',
+  } satisfies VariantList;
+
+  const roundedList = {
+    md: 'rounded-md',
+    lg: 'rounded-lg',
+    xl: 'rounded-xl',
+    '3xl': 'rounded-3xl',
+  } satisfies RoundedList;
+
+  export let variant: VariantKeyProp, rounded: RoundedKeyProp;
 </script>
 
-<button
-  class="{background
-    ? 'bg-white'
-    : 'bg-transparent'} px-8 py-3 rounded-3xl shadow-lg"
->
+<button class="{variantList[variant]} {roundedList[rounded]}">
   <slot />
 </button>
--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -1,4 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-500">
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1>this is the homepage</h1>
