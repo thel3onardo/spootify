43de0f2d9612aa393e9aa835056f1c7440bad3a8
thel3onardo <leonardo.contato03@gmail.com>
2023-09-15 12:55:06 -0300

    chore(client): create directive directory

--- apps/client/src/lib/components/collection/SearchBar.svelte
+++ apps/client/src/lib/components/collection/SearchBar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
-  import { clickOutside } from '$lib/utils/clickOutside';
+  import { clickOutside } from '$lib/directives/clickOutside';
+  import { fade, slide } from 'svelte/transition';
+  import { circOut } from 'svelte/easing';
 
   let visible = false;
   let value;
@@ -8,22 +10,20 @@
   const toggleVisible = () => {
     visible = !visible;
   };
-  const what = () => {
-    console.log('clicked outside');
-  };
 </script>
 
-<div class="text-gray-500">
+<div class="grid grid-cols-1 grid-rows-1 text-gray-500">
   {#if visible}
     <form
+      transition:slide={{ axis: 'x', easing: circOut }}
       use:clickOutside
-      on:clickOutside={what}
+      on:clickOutside={toggleVisible}
       class="flex items-center bg-gray-500/20 px-2 py-1 rounded"
     >
       <Icon
         icon="iconamoon:search-bold"
-        width="1rem"
-        height="1rem"
+        width="1.25rem"
+        height="1.25rem"
         class="mr-2"
       />
       <input
@@ -34,6 +34,7 @@
     </form>
   {:else}
     <button
+      transition:fade
       on:click={toggleVisible}
       class="p-2 rounded-full transition hover:bg-gray-500/20 hover:text-white"
     >
