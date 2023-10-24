15ae766ef04763d88b5a76980bb1c484091f4886
thel3onardo <leonardo.contato03@gmail.com>
2023-10-24 14:17:33 -0300

    feat(client): DropdownMenu component

---
+++ apps/client/src/lib/ui/components/DropdownMenu.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { createDropdownMenu, melt } from '@melt-ui/svelte';
+
+  const {
+    elements: { menu, item, trigger },
+    states: { open },
+  } = createDropdownMenu({ positioning: { placement: 'bottom-end' } });
+</script>
+
+<div use:melt={$trigger}>
+  <slot name="trigger" />
+</div>
+
+{#if $open}
+  <div use:melt={$menu} class={$$props.class}>
+    <slot name="options" />
+  </div>
+{/if}
