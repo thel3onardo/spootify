4940a14ee9ac833ea073b9bc8c553ae0310d062d
thel3onardo <leonardo.contato03@gmail.com>
2024-01-05 22:31:48 -0300

    client(fix): fix wrong click event emit on social auth button

--- apps/client/src/lib/ui/components/button/SocialAuthButton.svelte
+++ apps/client/src/lib/ui/components/button/SocialAuthButton.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  const emitClick = () => {
+    dispatch('click');
+  };
 
   export let icon: string;
 </script>
 
 <button
-  on:click={() => dispatchEvent(new MouseEvent('click'))}
+  on:click={emitClick}
   class="mb-4 flex w-full items-center justify-center rounded-3xl border border-transparent bg-gray-950 p-3 text-gray-400 hover:border-gray-700"
 >
   <Icon {icon} width="1.25rem" height="1.25rem" />
-  <span class="font-manrope ml-3 text-sm font-medium">
+  <span class="ml-3 font-manrope text-sm font-semibold">
     <slot />
   </span>
 </button>
