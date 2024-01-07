a32accb902f3057c9d3ba4a89fc9ededee5967c6
thel3onardo <leonardo.contato03@gmail.com>
2024-01-07 13:46:21 -0300

    feat(client): common X button component

---
+++ apps/client/src/lib/ui/components/button/X.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+
+  export let size: string;
+</script>
+
+<button
+  aria-label="close"
+  class="{$$props.class} flex h-8 w-8 cursor-default items-center justify-center rounded-full bg-neutral-900 hover:bg-neutral-800"
+>
+  <Icon icon="heroicons:x-mark-16-solid" width={size} height={size} />
+</button>
