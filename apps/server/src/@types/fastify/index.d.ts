8388f21635c342a2b9282b0d8a6f67958fcbd1a3
thel3onardo <leonardo.contato03@gmail.com>
2024-01-10 16:24:39 -0300

    server: alter env fields

--- apps/server/src/@types/fastify/index.d.ts
+++ apps/server/src/@types/fastify/index.d.ts
@@ -11,5 +11,12 @@ declare module "fastify" {
   > {
     session: Session | null;
     prisma: PrismaClient;
+    config: {
+      PORT: number;
+      JWT_SECRET: string;
+      DATABASE_URL: string;
+      IMAGEKIT_PUBLIC_KEY: string;
+      IMAGEKIT_PRIVATE_KEY: string;
+    };
   }
 }
