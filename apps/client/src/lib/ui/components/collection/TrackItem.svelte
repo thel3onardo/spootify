8c93397ab85da89657c8ba9a8c6f28e79448aa22
thel3onardo <leonardo.contato03@gmail.com>
2024-01-11 17:06:12 -0300

    client(feat): remove mock data in playlist track list

--- apps/client/src/lib/ui/components/collection/TrackItem.svelte
+++ apps/client/src/lib/ui/components/collection/TrackItem.svelte
@@ -1,35 +1,51 @@
 <script lang="ts">
-  //todo: Probably some functions to format data coming from the backend
+  import { secondsToMinutes } from '$lib/utils';
+
+  const formatDate = (date: string) => {
+    //date as ISO 8601
+    const dateFormat = new Date(date).toString().split(' ');
+    const day = dateFormat[1];
+    const month = dateFormat[2];
+    const year = dateFormat[3];
+
+    return `${day} ${month}, ${year}`;
+  };
+
+  export let name: string,
+    id: number,
+    coverImageUrl: string,
+    addedAt: string,
+    author: { name: string; id: string },
+    duration: number;
 </script>
 
-<div
-  class="grid h-[60px] grid-cols-[16px_6fr_4fr_3fr_1fr] items-center gap-4 rounded-lg px-4 font-inter text-sm text-neutral-400 hover:bg-neutral-800"
->
-  <div>
-    <span>1</span>
-  </div>
-  <div class="flex items-center">
-    <div class="mr-4 h-[45px] w-[45px] overflow-hidden rounded-lg">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/pt/5/52/Linkin_in_the_end_single_cover.png"
-        class="h-full object-cover"
-        alt="#"
-      />
+<button on:click={playTrack(id)} class="w-full">
+  <div
+    class="grid h-[60px] grid-cols-[16px_6fr_4fr_3fr_1fr] items-center gap-4 rounded-lg px-4 text-start font-inter text-sm text-neutral-400 hover:bg-neutral-800"
+  >
+    <div>
+      <span>1</span>
     </div>
-    <div class="text-sm">
-      <h4 class="mb-0 font-medium text-neutral-50">In The End</h4>
-      <span>
-        <a href="/" class="hover:underline">Linkin Park</a>
-      </span>
+    <div class="flex items-center">
+      <div class="mr-4 h-[45px] w-[45px] overflow-hidden rounded-lg">
+        <img src={coverImageUrl} class="h-full object-cover" alt="#" />
+      </div>
+      <div class="text-start text-sm">
+        <h4 class="mb-0 font-medium text-neutral-50">{name}</h4>
+        <span>
+          <a href="/" class="hover:underline">{author.name}</a>
+        </span>
+      </div>
+    </div>
+    <div>
+      <span>Starboy</span>
+    </div>
+    <div>
+      <span>{formatDate(addedAt)}</span>
+      <!-- <span>Aug 7, 2023</span> -->
+    </div>
+    <div>
+      <span>{secondsToMinutes(duration)}</span>
     </div>
   </div>
-  <div>
-    <span>Starboy</span>
-  </div>
-  <div>
-    <span>Aug 7, 2023</span>
-  </div>
-  <div>
-    <span>3:50</span>
-  </div>
-</div>
+</button>
--- apps/client/src/lib/ui/components/collection/TracksList.svelte
+++ apps/client/src/lib/ui/components/collection/TracksList.svelte
@@ -1,27 +1,46 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
   import TrackItem from './TrackItem.svelte';
+  import { number, string } from 'zod';
+
+  interface ITrack {
+    id: number;
+    name: string;
+    coverImage: string;
+    author: {
+      name: string;
+      id: string;
+    };
+    addedAt: string;
+    duration: number;
+  }
 
   const tableSections = ['#', 'Title', 'Album', 'Date added'];
+
+  export let tracks: ITrack[];
 </script>
 
 <div class="text-neutral-400 {$$props.class}">
   <div
-    class="mb-4 grid grid-cols-[16px_6fr_4fr_3fr_1fr] gap-4 border-b border-neutral-800 px-4 py-2 font-manrope text-sm font-medium"
+    class="font-manrope mb-4 grid grid-cols-[16px_6fr_4fr_3fr_1fr] gap-4 border-b border-neutral-800 px-4 py-2 text-sm font-medium"
   >
     {#each tableSections as section}
       <span>{section}</span>
     {/each}
-    <!-- TODO: maybe it would be better to render this Icon component in the above each block somehow.-->
     <Icon
       icon="material-symbols:alarm-outline"
       width="1.2rem"
       height="1.2rem"
     />
   </div>
-  <div>
-    {#each Array(10) as el}
-      <TrackItem />
-    {/each}
-  </div>
+  {#each tracks as track}
+    <TrackItem
+      id={track.id}
+      name={track.name}
+      coverImageUrl={track.coverImage}
+      author={track.author}
+      addedAt={track.addedAt}
+      duration={track.duration}
+    />
+  {/each}
 </div>
